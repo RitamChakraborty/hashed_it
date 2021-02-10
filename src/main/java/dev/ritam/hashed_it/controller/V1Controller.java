fcb4bb9cbc2fae973127e5d1b74564e713682515
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-02-10 12:06:00 +0530

    Autowire HashingService inside V1Controller

--- src/main/java/dev/ritam/hashed_it/controller/V1Controller.java
+++ src/main/java/dev/ritam/hashed_it/controller/V1Controller.java
@@ -1,21 +1,33 @@
 package dev.ritam.hashed_it.controller;
 
 import dev.ritam.hashed_it.model.HashedOutput;
+import dev.ritam.hashed_it.service.HashingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.NoSuchAlgorithmException;
 
 @RestController
 @RequestMapping("/api/v1")
 public class V1Controller {
+    private HashingService hashingService;
+
+    @Autowired
+    public void setHashingService(HashingService hashingService) {
+        this.hashingService = hashingService;
+    }
+
     @GetMapping("/{hashType}")
     public HashedOutput getHashedOutput(
             @PathVariable("hashType") String hashType,
             @RequestParam("input") String input
     ) {
-        HashedOutput hashedOutput = new HashedOutput();
-        hashedOutput.setInput(input);
-        hashedOutput.setOutput("hash(" + input + ")");
-        hashedOutput.setHashType(hashType);
+        try {
+            return hashingService.getHashedOutput(hashType, input);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
 
-        return hashedOutput;
+        return null;
     }
 }
